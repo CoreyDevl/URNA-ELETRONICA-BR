9a3a0f7fed1d9892e8e235b8f663a96d414c1dd3
Corey <90798397+CoreyTattooArt@users.noreply.github.com>
2021-10-23 12:08:18 -0300

    logica inicial inserida
    A logica inicial começa reconhecendo algumas tags do html
    depois disso predisponibilizamos as funções (sem conteúdo)
    em seguida a logica começa de fato pela função  começarEtapa()
    essa função limpa alguns itens do html para posteriormente poder inserir dados coletados do array de objetos do arquivo etapas.js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,30 @@
-let seuVotoPara = document.querySelector('d1left1 span')
-let cargo = document.querySelector('d1left2 span')
-let descriçao = document.querySelector('d1left4')
-let aviso = document.querySelector('d2')
-let lateral = document.querySelector('d1right')
-let numeros = document.querySelector('d1left3')
-
+let seuVotoPara = document.querySelector('.d1left1 span')
+let cargo = document.querySelector('.d1left2 span')
+let descriçao = document.querySelector('.d1left4')
+let aviso = document.querySelector('.d2')
+let lateral = document.querySelector('.d1right')
+let numeros = document.querySelector('.d1left3')
 
 let etapaAtual = 0
 
 function começarEtapa() {
-    let etapa = etapas[etapaAtual]
-    seuVotoPara.style.display = "none"
-    cargo.innerHTML = etapa.titulo
-    descricao.innerHTML = 
+    let etapa = etapas[etapaAtual];
+    let numeroHtml = `` ;
+     
+    for(let i = 0 ; i < etapa.numeros ; i ++){
+    numeroHtml += '<div class="numero"> </div>';    
+    
 
-}   //   CURSO EM 1 HORA E 9M
+    }
+
+    seuVotoPara.style.display = 'none';
+    cargo.innerHTML = etapa.titulo; 
+    descriçao.innerHTML = ``;
+    aviso.style.display = 'none';
+    lateral.innerHTML = ``;
+    numeros.innerHTML = numeroHtml;
+ }
+ 
 
 function clicou(n){
     alert("Clicou em" +n)
@@ -31,3 +41,5 @@ function corrige(){
 function confirma(){
     alert("Clicou em CONFIRMA")
 }
+
+começarEtapa();
